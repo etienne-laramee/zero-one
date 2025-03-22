b3d8ecc9aaf37e4fb8bba0a2cb7dfc9e0583f67e
Etienne <etienne.laramee@gmail.com>
2025-03-22 14:33:38 -0400

    Add factorial and WIP Combination endpoints

--- backend/app.py
+++ backend/app.py
@@ -3,24 +3,26 @@ from flask import Flask, request
 
 app = Flask(__name__)
 
-def addition(a, b):
-	return int(a)+int(b)
+def factorial(n):
+	if n > 0:
+		total = factorial(n-1) * n
+	else:
+		total = 1
+	return total
 
-@app.route('/')
-def hello():
-	return "Bonjour mate!"
+@app.get('/factorial')
+def fact():
+	n = int(request.args.get('n'))
+	_factorial = factorial(n)
+	return 'factorial {} = {}'.format(n, _factorial)
 
-@app.route('/expected-values')
+@app.get('/combination')
 def expectedValues():
-	return "Expected values: 123"
-
-@app.route('/addition')
-def add():
-	a = request.args.get('a')
-	b = request.args.get('b')
-	sum = addition(a, b)
-	message = f"The sum of {a} + {b} is {sum}!"
-	return message
+	n = int(request.args.get('n'))
+	r = int(request.args.get('r'))
+	print('n {}, r {}'.format(n, r))
+	combination = factorial(n)//(factorial(r)*factorial(n-r))
+	return "Combination {}, {} = {}".format(n, r, combination)
 
 if __name__ == '__main__':
 	app.run(host='0.0.0.0', port=5000, debug=True, use_reloader=True)
